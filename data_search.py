f3d39751cf0142bcd769e951cf0dd07f24606f1a
wavegate <cc.frankee@gmail.com>
2018-05-22 03:15:39 -0500

    updated data search

--- data_search.py
+++ data_search.py
@@ -1,8 +1,9 @@
 import requests
 from requests_html import HTMLSession
 import re
-
+import json
 import webbrowser
+import searches
 
 rates_data = requests.get("http://poe.ninja/api/Data/GetCurrencyOverview?league=Bestiary").json()
 parsed_rates_data = {}
@@ -34,14 +35,18 @@ if rates_data:
 
 
 armor_data = requests.get(UNIQUE_ARMOUR_PRICES_URL).json()
+weapon_data = requests.get(UNIQUE_WEAPON_PRICES_URL).json()
 #file = open('jsonviewer.txt', 'w', encoding='utf-8')
 #file.write(str(armor_data))
 #file.close()
-armor_values = {}
-if armor_data:
-	for unique_data in armor_data['lines']:
-		armor_values[unique_data['name']] = unique_data['chaosValue']
-
+values = {}
+#if armor_data:
+	#for unique_data in armor_data['lines']:
+		#values[unique_data['name']] = unique_data['chaosValue']
+#if weapon_data:
+#	for unique_data in weapon_data['lines']:
+#		values[unique_data['name']] = unique_data['chaosValue']
+#print(str(weapon_data))
 data = {
 	"league" : "Bestiary",
 	"name" : "Victario's Influence"
@@ -60,34 +65,23 @@ headers = {
 	"Accept-Language" : "en-US,en;q=0.9",
 	"Cookie" : "_ga=GA1.2.206788054.1525455159; __cfduid=dc212bce0ec374351dd9bcebeae36c5501525493980; league=Bestiary; _gid=GA1.2.2088975945.1526632014"
 }
+
 #league=Bestiary^&type=^&base=^&name=kaom^%^27s+heart^&dmg_min=^&dmg_max=^&aps_min=^&aps_max=^&crit_min=^&crit_max=^&dps_min=^&dps_max=^&edps_min=^&edps_max=^&pdps_min=^&pdps_max=^&armour_min=^&armour_max=^&evasion_min=^&evasion_max=^&shield_min=^&shield_max=^&block_min=^&block_max=^&sockets_min=^&sockets_max=^&link_min=^&link_max=^&sockets_r=^&sockets_g=^&sockets_b=^&sockets_w=^&linked_r=^&linked_g=^&linked_b=^&linked_w=^&rlevel_min=^&rlevel_max=^&rstr_min=^&rstr_max=^&rdex_min=^&rdex_max=^&rint_min=^&rint_max=^&mod_name=^&mod_min=^&mod_max=^&mod_weight=^&group_type=And^&group_min=^&group_max=^&group_count=1^&q_min=^&q_max=^&level_min=^&level_max=^&ilvl_min=^&ilvl_max=^&rarity=^&seller=^&thread=^&identified=^&corrupted=^&progress_min=^&progress_max=^&sockets_a_min=^&sockets_a_max=^&shaper=^&elder=^&map_series=^&crafted=^&enchanted=^&online=x^&altart=^&capquality=x^&buyout_min=^&buyout_max=^&buyout_currency=^&has_buyout=^&exact_currency=
 session = HTMLSession()
+#curl "http://poe.trade/search" -H "Connection: keep-alive" -H "Cache-Control: max-age=0" -H "Origin: http://poe.trade" -H "Upgrade-Insecure-Requests: 1" -H "Content-Type: application/x-www-form-urlencoded" -H "User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Referer: http://poe.trade/" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.9" -H "Cookie: _ga=GA1.2.206788054.1525455159; __cfduid=dc212bce0ec374351dd9bcebeae36c5501525493980; league=Bestiary; _gid=GA1.2.2088975945.1526632014" --data "league=Bestiary^&type=^&base=^&name=Kaom^%^27s+Heart^&dmg_min=^&dmg_max=^&aps_min=^&aps_max=^&crit_min=^&crit_max=^&dps_min=^&dps_max=^&edps_min=^&edps_max=^&pdps_min=^&pdps_max=^&armour_min=^&armour_max=^&evasion_min=^&evasion_max=^&shield_min=^&shield_max=^&block_min=^&block_max=^&sockets_min=^&sockets_max=^&link_min=^&link_max=^&sockets_r=^&sockets_g=^&sockets_b=^&sockets_w=^&linked_r=^&linked_g=^&linked_b=^&linked_w=^&rlevel_min=^&rlevel_max=^&rstr_min=^&rstr_max=^&rdex_min=^&rdex_max=^&rint_min=^&rint_max=^&mod_name=^%^23^%^25+increased+Fire+Damage^&mod_min=40^&mod_max=^&mod_weight=^&group_type=And^&group_min=^&group_max=^&group_count=1^&q_min=^&q_max=^&level_min=^&level_max=^&ilvl_min=^&ilvl_max=^&rarity=^&seller=^&thread=^&identified=^&corrupted=^&progress_min=^&progress_max=^&sockets_a_min=^&sockets_a_max=^&shaper=^&elder=^&map_series=^&crafted=^&enchanted=^&online=x^&altart=^&capquality=x^&buyout_min=^&buyout_max=^&buyout_currency=^&has_buyout=^&exact_currency=" --compressed
 
-searches = [
-	{
-		"league":"Bestiary",
-		"name":"Fenumus' Shroud",
-		"online":"x"
-	}
-]
-
-for armor in list(armor_values.keys()):
-	newsearch = {}
-	newsearch['league'] = 'Bestiary'
-	newsearch['name'] = armor
-	newsearch['online'] = 'x'
-	searches.append(newsearch)
-
-
+#print(searches[0]['mod_name'])
+#print(str(searches))
 #for search in searches:
 #	response = requests.post('http://poe.trade/search', headers=headers, data=search)
 #	webbrowser.open_new_tab(response.url)
-for search in searches:
+good_trades = []
+
+for search in searches.searches:
 	response = session.post('http://poe.trade/search', headers=headers, data=search)
 	#webbrowser.open_new_tab(response.url)
 	lines = response.html.find(".item")
 	prices = []
-	#print(search['name'])
 	for line in lines:
 		attributes = line.attrs
 		data_buyout = attributes['data-buyout']
@@ -99,8 +93,13 @@ for search in searches:
 				check = re.search(match, key, re.IGNORECASE)
 				if check:
 					amount_in_chaos = int(amount)*float(value)
+					#if amount_in_chaos < (values[search['name']] - 50):
+					good_trades.append(attributes)
 					prices.append(amount_in_chaos)
-	print(search['name'] + " | Average price in poe.trade: " + str(sum(prices)/len(prices)) + " #offers: " + str(len(prices)) + " | Price from poe.ninja: " + str(armor_values[search['name']]))
+	#print(str(search))
+	one = dict(search)['name']
+	if len(prices) > 0:
+		print(one + " | Average price in poe.trade: " + str(sum(prices)/len(prices)) + " #offers: " + str(len(prices)))
 
 	#webbrowser.open_new_tab(session.next
 #print(response.text)
